832e9745c5b099e89b121c046e73e8af8aaa9f1e
jetilling <jetilling@hotmail.com>
2017-02-23 22:53:02 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+var corsOptions = {
+  origin: 'http://localhost:4000'
+}
+
+var app = module.exports = express();
+
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use("/node_modules", express.static(path.resolve(__dirname, './node_modules')));
+app.use(express.static(__dirname + '/src'));
+
+app.listen(4000, function(){
+  console.log('This part works on ', 4000)
+})
