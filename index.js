64d98d7daaa2d09ee7599331b21f264f8a72b943
jetilling <jetilling@hotmail.com>
2017-03-06 07:20:40 -0700

    need to put everything in folders

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var corsOptions = {
   origin: 'http://localhost:4000'
 }
 
+function sendSpaFileIfUnmatched(req,res) {
+    res.sendFile("src/index.html", { root: '.' });
+}
+
 var app = module.exports = express();
 
 
@@ -14,6 +18,7 @@ app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use("/node_modules", express.static(path.resolve(__dirname, './node_modules')));
 app.use(express.static(__dirname + '/src'));
+app.use(sendSpaFileIfUnmatched);
 
 app.listen(4000, function(){
   console.log('This part works on ', 4000)
--- src/app/inventory.component.ts
+++ src/app/inventory.component.ts
@@ -1,69 +1,15 @@
 import { Component }        from '@angular/core';
 import { OnInit }           from '@angular/core';
+import { Router }           from '@angular/router';
+
 import { Inventory }        from './inventory';
 import { InventoryService } from './inventory.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-inventory',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>Selected Item</h2>
-  <ul class="inventory">
-    <li *ngFor="let item of inventory" [class.selected]="item === selectedInventory" (click)="onSelect(item)">
-      <span class="badge">{{item.id}}</span> {{item.name}} {{item.value}}
-    </li>
-  </ul>
-  <my-inventory-detail [inventory]="selectedInventory"></my-inventory-detail>
-`,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .inventory {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .inventory li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .inventory li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .inventory li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .inventory .text {
-      position: relative;
-      top: -3px;
-    }
-    .inventory .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `]
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
 })
 
 export class InventoryComponent implements OnInit {
@@ -75,10 +21,17 @@ export class InventoryComponent implements OnInit {
     this.selectedInventory = item;
   }
 
-  constructor(private inventoryService: InventoryService) {}
+  constructor(
+    private router: Router,
+    private inventoryService: InventoryService
+  ) {}
 
   getInventory(): void {
     this.inventoryService.getInventory().then(inventory => this.inventory = inventory);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedInventory.id]);
   }
 
   ngOnInit(): void {
